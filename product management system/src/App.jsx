1b9fab9b8b2f604b4139e75d34d65e788e7c1cb2
dongqiaoyue <dongqiaoyue98@gmail.com>
2023-12-15 22:25:17 -0500

    Header and Footer

--- product management system/src/App.jsx
+++ product management system/src/App.jsx
@@ -2,16 +2,20 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignIn from './Pages/SignIn/SignIn.jsx'
 import SignUp from './Pages/SignUp/SignUp.jsx'
+import Header from './Components/Header.jsx';
+import Footer from './Components/Footer.jsx';
 
 function App() {
 
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
         </Routes>
+        <Footer />
       </Router>
 
     </>
