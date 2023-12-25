95c30b3b16ad23852a9a68361ac7bad3d9a8d841
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-12-25 14:19:41 -0500

    create a load spinner component, and change home router to products

--- product management system/src/Pages/Error/Error.jsx
+++ product management system/src/Pages/Error/Error.jsx
@@ -1,16 +1,20 @@
 import './Error.css'
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { useNavigate } from 'react-router-dom';
 
 function Error() {
-	// TODO: router to home page when click button
+  let navigate = useNavigate();
+	const handleClick = () => {
+    navigate('/')
+  }
 
   return (
     <>
-        <div className="error-container">
-					<ErrorOutlineIcon style={{ color: '#5b6af5', fontSize: 80 }} />
-          <p className='description'>Oops, something went wrong!</p>
-          <button className='home-button'>Go Home</button>
-        </div>
+      <div className="error-container">
+        <ErrorOutlineIcon style={{ color: '#5b6af5', fontSize: 80 }} />
+        <p className='description'>Oops, something went wrong!</p>
+        <button className='home-button' onClick={handleClick}>Go Home</button>
+      </div>
     </>
   )
 }
