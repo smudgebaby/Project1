be8abc229c469f1098548c6deb61957c213d41a2
dongqiaoyue <dongqiaoyue98@gmail.com>
2024-01-08 23:23:37 -0500

    Add coupon, product crud.

--- product management system/src/Pages/SignIn/SignIn.jsx
+++ product management system/src/Pages/SignIn/SignIn.jsx
@@ -1,61 +1,33 @@
 import Layout from '../Layout/Layout';
 import { Link } from 'react-router-dom';
 import './SignIn.css'
-import {signInUser} from '../../Utils/backendUtil.js';
-import {useDispatch} from 'react-redux';
-import {userSlice} from '../../Store/userSlice';
-import {useEffect, useState} from 'react';
 
-function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordShow, setPasswordShow] = useState(false);
+const SignIn = () => {
+  const additionalLinks =
+    <>
+      <p>Don&apos;t have an account? <Link to='/signup'>Sign up</Link></p>
+      <Link to='/resetpassword' className='reset-password'>Forgot password?</Link>
+    </>
+  ;
 
-  const dispatch = useDispatch();
+  const isValidEmail = () => {
+    return true;
+  };
 
-  useEffect(() => {
-    console.log(passwordShow);
-  }, [passwordShow])
+  const isValidPassword = () => {
+    return true;
+  };
 
-  const togglePasswordShow = () => {
-    setPasswordShow(!passwordShow)
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const user = await signInUser();
-    console.log(user);
-    dispatch(userSlice.actions.setCurrentUser(user));
-  }
-
-  const SignIn = () => {
-    const additionalLinks =
-      <>
-        <p>Don&apos;t have an account? <Link to='/signup'>Sign up</Link></p>
-        <Link to='/resetpassword' className='reset-password'>Forgot
-          password?</Link>
-      </>
-    ;
-
-    const isValidEmail = () => {
-      return true;
-    };
-
-    const isValidPassword = () => {
-      return true;
-    };
-
-    return (
-      <Layout
-        status='signin'
-        title="Sign in to your account"
-        buttonText="Sign In"
-        additionalLinks={additionalLinks}
-        isValidEmail={isValidEmail}
-        isValidPassword={isValidPassword}
-      />
-    );
-  }
-}
+  return (
+    <Layout
+      status='signin'
+      title="Sign in to your account"
+      buttonText="Sign In"
+      additionalLinks={additionalLinks}
+      isValidEmail={isValidEmail}
+      isValidPassword={isValidPassword}
+    />
+  );
+};
 
 export default SignIn;
