6bf01ce79b0abca9f781ac04a78b7a9e3f301ffa
Damon <damon.liren@gmail.com>
2024-01-11 15:47:27 -0800

    pagination

--- product management system/backend/routes/product.js
+++ product management system/backend/routes/product.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import productsController from '../controllers/products.js';
-const {createProduct, getAllProducts, getProduct, updateProduct} = productsController;
+const {createProduct, getAllProducts, getProduct, updateProduct, getPage} = productsController;
 
 router.post('/create', createProduct);
 router.get('/getById/:id', getProduct);
 router.get('/getAll', getAllProducts);
 router.post('/update/:id', updateProduct);
+router.get('/page/:page/:sort', getPage);
 
 export default router;
